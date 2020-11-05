ce13b793eb3c2fc6087bec161118280802fed391
oksovskii <21ololoha@gmail.com>
2020-11-06 02:24:55 +0300

    tg->vk

--- main.py
+++ main.py
@@ -1,33 +1,10 @@
-import requests
-import telebot
-import func as f
-import const as c
-from datetime import datetime
+import const
+from vkwave.bots import SimpleLongPollBot, SimpleBotEvent
 
-#Token of your bot
-bot = telebot.TeleBot(c.token)
+bot = SimpleLongPollBot(tokens=const.token, group_id=const.id)
 
-bot.send_message(681875938, 'Im started at {%s} (UTC+0)' %
-                 str(datetime.strftime(datetime.now(), "%H:%M:%S")))
+@bot.message_handler(bot.text_filter(("расписание", "Расписание")))
+def echo(event: SimpleBotEvent) -> str:
+    return "hello!"
 
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    msg = bot.send_message(
-        message.chat.id, 'Привет! Я присылаю расписание для "ВКСиИТ"!\n Напиши {/help} для подробной информации.')
-
-
-@bot.message_handler(commands=['help'])
-def send_message(message):
-    msg = bot.send_message(
-        message.chat.id, 'Использование: {/rasp} - запрос расписания НПО.')
-
-
-@bot.message_handler(commands=['rasp'])
-def send_photo(message):
-    f.schelude()
-    msg = bot.send_message(message.chat.id, 'Расписание:>')
-    bot.send_photo(message.chat.id, photo=open('img/rasp-1.jpg', 'rb'))
-
-
-bot.polling()
+bot.run_forever()
